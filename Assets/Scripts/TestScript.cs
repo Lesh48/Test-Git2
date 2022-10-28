541dd7ba721df4c6f164bb71ea3805123b8ee214
Lesh48 <leka122804@gmail.com>
2022-10-28 15:56:09 -0600

    t

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //hi
+        //hi, there
     }
 
     // Update is called once per frame
